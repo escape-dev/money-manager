1714eac683f24371745e9c23fa13d200c78b1fba
Muhammad Hafiz <hafiz170301@gmail.com>
2026-07-14 15:59:58 +0700

    feat: create show endpoint for category

--- app/controllers/api/v1/categories_controller.rb
+++ app/controllers/api/v1/categories_controller.rb
@@ -1,5 +1,6 @@
 class Api::V1::CategoriesController < ApplicationController
   before_action :authenticate_request!
+  before_action :set_category, only: %i[ show ]
   
   def index
     @categories = Category.where(user_id: @current_user.id)
@@ -17,9 +18,19 @@ class Api::V1::CategoriesController < ApplicationController
     end
   end
 
+  def show 
+    response_ok(@category, :ok)
+  end
+
   private 
 
   def category_params
     params.permit(:name, :icon)
+  end
+
+  def set_category 
+    @category = Category.find_by(id: params[:id], user_id: @current_user.id)
+
+    response_error("Category not found", :not_found) unless @category
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,7 @@ Rails.application.routes.draw do
         get :me, on: :collection
       end
 
-      resources :categories, only: %i[ index create ]
+      resources :categories, only: %i[ index create show ]
     end
   end
 end
